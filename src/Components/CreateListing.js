2c21a7c2790d34abe388923e4d86011d92c6104b
ElektrosStulpas <aurimas0pupsys@gmail.com>
2022-04-24 14:28:51 +0300

    Create listing functionality almost done, picture upload done. A lot of things missing if we were building a full system, like input validation. One thing that needs to be sorted out is POST due to backend and frontend object property expectations. On the same note need to implement some sort of system to come up with listingId and getting username for owner field

--- src/Components/CreateListing.js
+++ src/Components/CreateListing.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
+import PicUpload from './PicUpload';
 
 const CreateListing = () => {
     var initialState = {
+        listingId: 0,
+        ownerUsername: '',
         title: '',
         price: '',
         city: '',
@@ -12,16 +15,17 @@ const CreateListing = () => {
         phone: '',
         email: '',
         description: '',
-        startDate: new Date(),
-        endDate: null,
+        startDate: '',
+        endDate: '',
+        pic: null
     }
 
     const [state, setState] = useState(initialState);
 
     var navigate = useNavigate();
 
-    var handleChange = (event) => {
-        const {name, value} = event.target
+    var handleChange = (name, value) => {
+        // const {name, value} = event.target
 
         //TODO possible to do input validation here?
         setState({
@@ -30,8 +34,34 @@ const CreateListing = () => {
         })
     }
 
+    async function postData(url = '', data) {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: data
+        });
+    
+        if(response.status === 200)
+        {
+            // locationUrl = response.headers.get('Location');
+            // locationUrl = locationUrl.replace('http', 'https');
+            return response.json(); // parses JSON response into native JavaScript objects
+        }
+        else
+        {
+            throw new Error("Something went wrong when sending POST request. Status got: " + response.status);
+        }
+      }
+
     var handleSubmit = () => {
-        // TODO api POST handling here
+        // postData('https://gariunaicloud.azurewebsites.net/api/Listings', JSON.stringify(state))
+        //         .then(response => {
+        //         console.log(response);
+        //         }).catch(error => {
+        //             console.error(error);
+        //         });
         navigate("/")
     }
 
@@ -41,6 +71,12 @@ const CreateListing = () => {
       const [start, end] = dates;
       setStartDate(start);
       setEndDate(end);
+
+      setState({
+            ...state,
+            startDate: start.toJSON(),
+            endDate: end === null ? null : end.toJSON(),
+        })
     };
 
     return (
@@ -53,61 +89,61 @@ const CreateListing = () => {
                     name="title"
                     id="title"
                     value={state.title}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="price">Price/day</label>
                 <input
                     type="text"
                     name="price"
                     id="price"
                     value={state.price}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="city">City</label>
                 <input
                     type="text"
                     name="city"
                     id="city"
                     value={state.city}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="collateral">Collateral</label>
                 <input
                     type="text"
                     name="collateral"
                     id="collateral"
                     value={state.collateral}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="phone">Phone</label>
                 <input
                     type="text"
                     name="phone"
                     id="phone"
                     value={state.phone}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="email">Email</label>
                 <input
                     type="text"
                     name="email"
                     id="email"
                     value={state.email}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="description">Description</label>
-                <input
-                    type="textarea"
+                <textarea
                     name="description"
                     id="description"
                     value={state.description}
-                    onChange={handleChange} />
+                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
                 <label htmlFor="datepicker">Pick availability frame:</label>
                 <DatePicker
-                    name="datepicker"
-                    selected={state.startDate}
-                    onChange={handleChange}
-                    startDate={state.startDate}
-                    endDate={state.endDate}
+                    selected={startDate}
+                    onChange={onChange}
+                    startDate={startDate}
+                    endDate={endDate}
                     selectsRange
                     inline
                 />
-                <input type="button" value="Submit" onClick={handleSubmit}/>
+                <input type="button" value="Create the listing" onClick={handleSubmit}/>
             </form>
+            <button onClick={() => {navigate('/')}}>Cancel</button>
+            <PicUpload currentState={[state, setState]} handleChange={handleChange}/>
         </div>
     )
 }
