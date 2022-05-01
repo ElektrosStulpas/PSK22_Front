9c4a6997955055184cc076f59120ec9f54bf5b44
tautvilis <tautvilis@gmail.com>
2022-05-02 00:29:14 +0300

    Added simple listings page and changed navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react"
 
-import Stack from 'react-bootstrap/Stack'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
+import {Button,Card,Container,Row,Col,Form, Pagination} from 'react-bootstrap';
 
 const Home = () => {
     const [listings, setListing] = useState([])
+    const [query, setQuery] = useState("")
+
+    var days = 5
 
     const fetchData = () => {
         fetch("https://gariunaicloud.azurewebsites.net/api/Listings")
@@ -25,30 +22,45 @@ const Home = () => {
     useEffect(() => {
         fetchData()
     }, [])
+
+    const form = 
+    <Form>
+        <Form.Group className="w-50 mx-auto py-2" controlId="search">
+        <Form.Control type="string" onChange={event => setQuery(event.target.value)} placeholder="Search for anything" />
+        </Form.Group>
+    </Form>
+
+    function calcPrice(dayPrice, days) {
+        return dayPrice * days
+    }
+
     
     return (
+        <Container>
         <div className="home">
-            <h1>RenTool homepage</h1>
-            <Link to="/create-listing">
-                <Button variant="primary" >
-                    Create listing
-                </Button>
-            </Link>
-                <Container>
+            <h1 style={{textAlign:'center'}}>Listings</h1>
+            {form}
+                
                 {listings.length > 0 && (
                     <Row xs={2} md={4}>
-                        {listings.map(listing => (
+                        {listings.filter(listing => {
+                            if (query === '') {
+                                return listing;
+                            } else if (listing.title.toLowerCase().includes(query.toLowerCase())) {
+                                return listing;
+                            }
+                        }).map(listing => (
                             <Col key={listing.listingId} style={{marginBottom:'10px'}}>
                                 <Card style={{ width: '18rem',height:'100%'}}>
-                                <Card.Img variant="top" src="https://via.placeholder.com/200x200" />
                                 <Card.Body className="d-flex flex-column">
-                                    <Card.Title>{listing.title}</Card.Title>
-                                    <Card.Subtitle className="mb-2">Price: {listing.daysPrice}€</Card.Subtitle>
-                                    <Card.Subtitle classname="mb-2">City: {listing.city}</Card.Subtitle>
-                                    <Card.Text>
-                                    {listing.description}
-                                    </Card.Text>
-                                    <Button variant="primary" className="mt-auto">Open listing</Button>
+                                    <Card.Title style={{textAlign:'center'}}>{listing.title}</Card.Title>
+                                        <Card.Img variant="top" src="https://via.placeholder.com/200x200" /> 
+                                        <Card.Text disabled>{listing.description}</Card.Text> 
+                                    <Row>
+                                        <Col><Card.Subtitle className="mb-2" >{listing.daysPrice}€/day</Card.Subtitle></Col>
+                                        <Col><Card.Subtitle className="mb-2 h6 small totalPrice" >Total: {calcPrice(listing.daysPrice,days)}€</Card.Subtitle> </Col>
+                                    </Row>
+                                    <Card.Subtitle className="mb-2 city">City: {listing.city}</Card.Subtitle>
                                 </Card.Body>
                                 </Card>
                             </Col> 
@@ -56,8 +68,8 @@ const Home = () => {
                     </Row>
                     
                 )}
-                </Container>
         </div>
+        </Container>
     )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+import {Navbar, Nav, Container} from 'react-bootstrap'
+
+
+const navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="home_logo">
-                <h1 >
-                    <Link to="/">RenTool</Link>
-                </h1>
-            </div>
-            <div className="links">
-                <Link to="/login">
-                    <button type="button">
-                        Log in
-                    </button>
-                </Link>
-                <Link to="/signup">
-                    <button type="button">
-                        Sign up
-                    </button>
-                </Link>
-            </div>
-        </nav>
+        <Navbar bg="white" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/" className="ms-auto">
+                    <img src={process.env.PUBLIC_URL + '/logo.png'} alt="RenTool"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mx-auto">
+                    <Nav.Link as={Link} to="/create-listing">Create a listing</Nav.Link>
+                </Nav>
+                <Nav className="me-1">
+                    <Nav.Link as={Link} to="/login">Log in</Nav.Link>
+                    <Nav.Link as={Link} to="/signup">Sign up</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
-export default Navbar;
+export default navbar;
