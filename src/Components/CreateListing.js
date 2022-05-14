4949e1c30c4912abcf98a3b6c613067e79bbe4de
Martynas Cibulskis <martynasc@unifysquare.com>
2022-05-15 00:31:07 +0300

    Add images

--- src/Components/CreateListing.js
+++ src/Components/CreateListing.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
-import PicUpload from './PicUpload';
 import axios from 'axios';
 import authHeader from "../services/auth-header";
 const API_URL = 'https://gariunaicloud.azurewebsites.net/api/Listings/';
@@ -14,51 +12,46 @@ const CreateListing = () => {
         city: '',
         deposit: 0,
         description: '',
-    }
-
-    var otherState = {
-        startDate: '',
-        endDate: '',
-        pic: null
+        image: null
     }
 
     const [state, setState] = useState(listingState);
-    const [tempState, setTempState] = useState(otherState);
 
     var navigate = useNavigate();
 
     var handleChange = (name, value) => {
-
-        //TODO possible to do input validation here?
+        console.log(name, value)
         setState({
             ...state,
             [name]: value,
         })
     }
 
-    var handleTempChange = (name, value) => {
-
-        setTempState({
-            ...state,
-            [name]: value,
-        })
-    }
-
     const registerListing = () => {
-        return axios.post(API_URL, state, { headers: authHeader() })
+        var payload = {
+            title: state.title,
+            daysPrice: state.daysPrice,
+            city: state.city,
+            deposit: state.deposit,
+            description: state.description,
+        }
+        return axios.post(API_URL, payload, { headers: authHeader() })
             .then((response) => {
                 console.log(response)
                 return response
             });
     }
 
+    const updateListingImage = (listingId, image) => {
+        const formData = new FormData()
+        formData.append("file", image)
+        return axios.put(API_URL + `${listingId}/image`, formData, { headers: authHeader() });
+    }
+
     var handleSubmit = () => {
-        registerListing().then(
-            (response) => {
-                console.log(response.data)
-                navigate("/")
-            },
-            (error) => {
+        registerListing()
+            .then(response => response.data)
+            .catch(error => {
                 const resMessage =
                     (error.response &&
                         error.response.data &&
@@ -66,75 +59,100 @@ const CreateListing = () => {
                     error.message ||
                     error.toString();
                 console.log(resMessage)
-            }
-        )
+            })
+            //image uploading will fail silently px yr
+            .then(newListingId => {
+                if (state.image != null) {
+                    return updateListingImage(newListingId, state.image)
+                }
+            })
+            .then(_ => navigate("/"))
     }
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(null);
-    const onChange = (dates) => {
-        const [start, end] = dates;
-        setStartDate(start);
-        setEndDate(end);
-
-        setTempState({
-            ...state,
-            startDate: start.toJSON(),
-            endDate: end === null ? null : end.toJSON(),
-        })
-    };
+    const imagePreview = () => {
+        return state.image ? <img src={URL.createObjectURL(state.image)} /> : <div />
+    }
 
     return (
-        <div className='createListingForm'>
-            <h1>Listing creation</h1>
-            <form>
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    value={state.title}
-                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
-                <label htmlFor="daysPrice">Price/day</label>
-                <input
-                    type="number"
-                    name="daysPrice"
-                    id="daysPrice"
-                    value={state.daysPrice}
-                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
-                <label htmlFor="city">City</label>
-                <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    value={state.city}
-                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
-                <label htmlFor="deposit">Deposit</label>
-                <input
-                    type="number"
-                    name="deposit"
-                    id="deposit"
-                    value={state.deposit}
-                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
-                <label htmlFor="description">Description</label>
-                <textarea
-                    name="description"
-                    id="description"
-                    value={state.description}
-                    onChange={(event) => handleChange(event.target.name, event.target.value)} />
-                <label htmlFor="datepicker">Pick availability frame:</label>
-                <DatePicker
-                    selected={startDate}
-                    onChange={onChange}
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectsRange
-                    inline
-                />
-                <input type="button" value="Create the listing" onClick={handleSubmit} />
+
+        <div className="new-listing-form-container">
+            <h1>Create new listing</h1>
+            <form class='new-listing-form' onSubmit={e => {e.preventDefault(); handleSubmit()}}>
+
+                <div className="form-element">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        value={state.title}
+                        required
+                        onChange={(event) => handleChange(event.target.name, event.target.value)} />
+                </div>
+
+                <div className="form-element">
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        name="city"
+                        id="city"
+                        required
+                        value={state.city}
+                        onChange={(event) => handleChange(event.target.name, event.target.value)} />
+                </div>
+
+                <div className="form-element">
+                    <label htmlFor="daysPrice">Price/day</label>
+                    <input
+                        type="number"
+                        name="daysPrice"
+                        id="daysPrice"
+                        required
+                        value={state.daysPrice}
+                        onChange={(event) => handleChange(event.target.name, event.target.value)} />
+                </div>
+
+                <div className="form-element">
+                    <label htmlFor="deposit">Deposit</label>
+                    <input
+                        type="number"
+                        name="deposit"
+                        id="deposit"
+                        required
+                        value={state.deposit}
+                        onChange={(event) => handleChange(event.target.name, event.target.value)} />
+
+                </div>
+
+                <div className="form-element">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        name="description"
+                        id="description"
+                        required
+                        value={state.description}
+                        onChange={(event) => handleChange(event.target.name, event.target.value)} />
+                </div>
+
+                <div className="form-element">
+                    <label htmlFor='image'>Picture</label>
+                    <input
+                        nameid="image"
+                        id="image"
+                        type="file"
+                        onChange={(event) => {
+                            handleChange("image", event.target.files[0])
+                        }} />
+                    {imagePreview()}
+                </div>
+
+                <div class="form-element">
+                    <input type="submit" value="Create the listing"/>
+                </div>
+
             </form>
             <button onClick={() => { navigate('/') }}>Cancel</button>
-            <PicUpload currentState={[tempState, setTempState]} handleChange={handleTempChange} />
+
         </div>
     )
 }
