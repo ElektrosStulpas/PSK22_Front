8b1982e2778c0cf39e08499972e17f621b7a762f
ElektrosStulpas <aurimas0pupsys@gmail.com>
2022-05-17 17:42:48 +0300

    added bootstrap buttons in MyListings, but they seem to be bugged, tried searching for solutions, but haven't found any, leaving as is for now

--- src/Components/MyListings.js
+++ src/Components/MyListings.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import authHeader from "../services/auth-header";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import { UserContext } from "../services/UserContext";
 const API_URL = 'https://gariunaicloud.azurewebsites.net/api/Listings/';
 
@@ -62,9 +62,10 @@ const MyListings = () => {
                                 <td key={listing.id}>{listing.title}</td>
                                 <td key={listing.id}>{listing.city}</td>
                                 <td key={listing.id}>{listing.description}</td>
-                                <button onClick={() => { navigate('/edit-listing', { state: listing }) }}>Edit</button>
-                                <button onClick={() => { deleteListing(listing) }}>Delete</button>
-                            </tr>)
+                                <Button variant="primary" onClick={() => { navigate('/edit-listing', { state: listing }) }}> Edit </Button>
+                                <Button variant="secondary" onClick={() => { deleteListing(listing) }}> Delete </Button>
+                            </tr>
+                        )
                     )}
                 </tbody>
             </Table>
