4549b17eee485dbf8571a13d62e3c2240da453ac
tautvilis <tautvilis@gmail.com>
2022-05-26 00:13:41 +0300

    Done all tasks

--- src/Components/ListingCard.js
+++ src/Components/ListingCard.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react"
 import { Card } from 'react-bootstrap';
 
 export const ListingCard = (listing) => {
-    console.log(listing)
     const loadingGif = process.env.PUBLIC_URL + '/loading.gif'
     const defaultImage = process.env.PUBLIC_URL + '/no_image.png'
     const realImageUrl = `http://gariunaicloud.azurewebsites.net/api/Listings/${listing.listingId}/image`
@@ -28,7 +27,7 @@ export const ListingCard = (listing) => {
         <Card style={{ width: '18rem', height: '100%' }}>
             <Card.Body className="d-flex flex-column">
                 <Card.Title style={{ textAlign: 'center' }}>{listing.title}</Card.Title>
-                <Card.Img style={{ 'object-fit': 'scale-down', 'height': '15rem' }} variant="top" src={imageUrl} />
+                <Card.Img style={{ 'objectFit': 'scale-down', 'height': '15rem' }} variant="top" src={imageUrl} />
                 {/* <Card.Text disabled>{listing.description}</Card.Text> */}
                 <Card.Subtitle className="mb-3 daysPrice" >{listing.daysPrice}€ / day</Card.Subtitle>
                 <Card.Subtitle className="mb-3 totalPrice" >Total: {calcPrice(listing.daysPrice, days).toFixed(2)}€</Card.Subtitle>
